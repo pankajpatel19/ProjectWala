f85c7b30460d178d7228d441b1b0d9f7ebaecf05
Pankajpatel19 <pankajpatelinter@gmail.com>
2026-01-08 00:11:23 +0530

    add role access middleware to project upload route

--- src/routes/project/project.routes.js
+++ src/routes/project/project.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import upload from "../../middleware/multer.middleware.js";
 import { createProjectService } from "../../controllers/project/project.controller.js";
 import { authMiddleware } from "../../middleware/AuthMiddleware.js";
+import { roleAccess } from "../../middleware/roleAccess.middleware.js";
 const router = Router();
 
 router.post(
   "/upload-project",
   authMiddleware,
+  roleAccess("seller"),
   upload.single("projectImage"),
   createProjectService
 );
